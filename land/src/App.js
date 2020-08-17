6b4b54161a7e144f20c7d97e845734c340523028
liangliangMirror <jxliangweihan@gmail.com>
2020-08-17 16:28:57 +0800

    sss

--- land/src/App.js
+++ land/src/App.js
@@ -1,10 +1,21 @@
-import React from 'react';
-import './App.css';
-import { Switch,withRouter } from 'react-router-dom'
+import React from 'react'
+import './App.css'
+import { Switch, withRouter } from 'react-router-dom'
 // import PrivateRoute from '@/components/PrivateRoute'
 import LoadableComponent from '@/utils/loadableComponent'
 const Index = LoadableComponent(import('@/page/index'))
 // @withRouter
+const effect = function rea(fn:()=>{},args) {
+  return run(effect, args)
+}
+effect.deps = ['123']
+effect.active = true
+function run(effect, args) {
+  const { deps } = effect
+  console.log(effect.active)
+  console.log(deps, 1111111)
+}
+new effect()
 class App extends React.Component {
   render(h) {
     return (
@@ -13,8 +24,8 @@ class App extends React.Component {
       //   <PrivateRoute path='/' component={Index} />
       // </Switch>
       <Index />
-    );
+    )
   }
 }
 
-export default App;
+export default App
